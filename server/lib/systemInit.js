a05dbb8c29343c58f3e893248a56c1414ec3d21d
murvinlai <murvinlai@gmail.com>
2013-12-15 02:14:03 -0800

    read and pass fields from build_config to config

--- server/lib/systemInit.js
+++ server/lib/systemInit.js
@@ -11,11 +11,11 @@ module.exports.init = function () {
 	global.build_config = require(cfg.build_config_path + cfg.build_config_name).config;
 
 	// port specific thing from build_config to cfg
-	cfg.build_app_domain = build_config.build_app_domain;
-	cfg.build_app_port = build_config.build_app_port;
-	cfg.build_file_root_path = build_config.build_file_root_path;
-	cfg.build_file_name = build_config.build_file_name;
-	cfg.build_admin = build_config.build_admin;
+	for (key in build_config) {
+		if (build_config.hasOwnProperty(key)) {
+			cfg[key] = build_config[key];
+		}
+	}
 	
 	cfg.session_secret = build_config.session_secret;
 }
