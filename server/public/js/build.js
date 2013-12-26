b276c1ac341eba7490590015b8ac0dff9fddc7f6
murvinlai <murvinlai@gmail.com>
2013-12-26 06:29:14 -0800

    add js init

--- server/public/js/build.js
+++ server/public/js/build.js
@@ -2,8 +2,33 @@
  * Build.js
  */
 
-if (_XUKA_) {
-	alert('xuka exist');
-} else {
-	alert('xuka not exist');
+if (typeof(_XUKA_) == 'undefined') {
+	_XUKA_= {};
+} 
+
+_XUKA_.init = function () {
+	// bind inputs
+	for (key in _XUKA_.input) {
+		if (_XUKA_.input.hasOwnProperty(key)) {
+			$(key)[_XUKA_.input[key].action_type](_XUKA_.input[key].call);
+		}
+	}
 }
+
+_XUKA_.input = {
+	build_now : {
+		action_type: 'click',
+		call: function(){
+			console.log("Build now");
+		}
+	},
+	select_log: {
+		action_type: 'click',
+		call: function(){
+			console.log("Select Log now");
+		}
+	}
+}
+
+
+_XUKA_.init();
