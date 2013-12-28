aa7a02cda8e03a3532469e7b2197a864875064aa
murvinlai <murvinlai@gmail.com>
2013-12-28 01:19:35 -0800

    Build JS and actionManager

---
+++ server/lib/actionManager.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+
+module.exports.scriptExec = function(data, next) {
+	console.log("Script Exec");
+	
+	var otherParamsStr = ""; // write to log??
+	var script = path.resolve(cfg.build_file_root_path + cfg.build_file_name + " " + otherParamsStr);
+	
+	console.log("running script " + script);
+	
+	exec(script, function(err, stdout, stderr){
+		console.log(stdout);
+        console.log(stderr);
+        console.log(err);
+        var result = {
+            stdout:stdout,
+            stderr:stderr,
+            error:err
+        }
+        next(null, result);
+	});
+}
+
+
+
+module.exports.publish = function (data, next) {
+    var output = {};
+
+    var fromfile = workspaceRoot + "/" + fileName;
+    var tofile = repoRoot + "/data/stl/" + fileName;
+
+    var fs = require('fs');
+
+    fs.createReadStream(fromfile).pipe(fs.createWriteStream(tofile));
+
+    var path = require("path");
+    var script = path.resolve(__dirname + "/../scripts/push.sh " + repoRoot);
+
+    console.log("running script " + script);
+
+    var exec = require('child_process').exec
+
+    exec(script, function(error, stdout, stderr){
+
+        console.log(stdout);
+        console.log(stderr);
+        console.log(error);
+        var result = {
+            stdout:stdout,
+            stderr:stderr,
+            error:error
+        }
+        next(null, result)
+        g_io.sockets.emit('publish', {status:true, data:output});   // annouce to whole world
+        console.log("Emit Publish");
+    })
+}
+
--- server/public/js/build.js
+++ server/public/js/build.js
@@ -20,12 +20,31 @@ _XUKA_.input = {
 		action_type: 'click',
 		call: function(){
 			console.log("Build now");
+			$.ajax({
+				type: "POST", 
+				url: _XUKA_.url.build + "/build",
+				data: {_csrf:_XUKA_.csrftoken},
+				success: function(data) {
+					console.log(data);
+				},
+				dataType: 'json'
+			});
 		}
 	},
 	select_log: {
 		action_type: 'click',
 		call: function(){
 			console.log("Select Log now");
+			var log_id = 0;
+			$.ajax({
+				type: "POST", 
+				url: _XUKA_.url.build + "/load_log",
+				data: {_csrf:_XUKA_.csrftoken, log_id:log_id},
+				success: function(data) {
+					console.log(data);
+				},
+				dataType: 'text'
+			});
 		}
 	}
 }
