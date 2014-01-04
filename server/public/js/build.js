7b69cec7d73516a1399c80ceb5b8852331fe6aaf
murvinlai <murvinlai@gmail.com>
2014-01-04 02:56:32 -0800

    show log data

--- server/public/js/build.js
+++ server/public/js/build.js
@@ -46,7 +46,7 @@ _XUKA_.input = {
 				data: {_csrf:_XUKA_.csrftoken, fileName:selectedValue},
 				success: function(data) {
 					console.log(data);
-					$('#logContentTextArea').val(data);
+					$('#logContentTextArea').val(data.logContent);
 				},
 				dataType: 'text'
 			});
