e5adedd511a5ab49f211c0fb82349547174fd888
murvinlai <murvinlai@gmail.com>
2014-01-07 02:07:48 -0800

    stupid csrf

--- server/server.js
+++ server/server.js
@@ -93,7 +93,9 @@ server.use(express.session({
 }));
 
 server.use(express.csrf());
+
 server.use(function (req, res, next) {
+	res.cookie('XSRF-TOKEN', req.csrfToken());
 	res.locals.csrftoken = req.csrfToken();
     next();
 });
